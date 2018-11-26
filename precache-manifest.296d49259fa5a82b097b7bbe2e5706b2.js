477b42a0ee099c2d46181f8b92b46983377f5b0a
Mariusz Michalak <stricte@gumed.edu.pl>
2018-11-26 23:01:00 +0100

    Updates

---
+++ precache-manifest.296d49259fa5a82b097b7bbe2e5706b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9083da2929556be85a39",
+    "url": "/react-minesweeper/static/css/main.3fd49ffa.chunk.css"
+  },
+  {
+    "revision": "9083da2929556be85a39",
+    "url": "/react-minesweeper/static/js/main.9083da29.chunk.js"
+  },
+  {
+    "revision": "f9d8d94592249a6b2f17",
+    "url": "/react-minesweeper/static/js/1.f9d8d945.chunk.js"
+  },
+  {
+    "revision": "1c80af3cb660a53b4a2e",
+    "url": "/react-minesweeper/static/js/runtime~main.1c80af3c.js"
+  },
+  {
+    "revision": "e58e5a646f249eee7dcedcc24de12480",
+    "url": "/react-minesweeper/index.html"
+  }
+];
